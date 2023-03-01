0d2060f69eef4211922e92a701bb05e384cc3aac
CK <73201351+shdw9@users.noreply.github.com>
2023-03-01 15:40:14 -0800

    Finished task h - MouseDropper

--- Assets/MouseDropper.cs
+++ Assets/MouseDropper.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Reflection;
 using UnityEngine;
 
 public class MouseDropper : MonoBehaviour
 {
     private Camera cam;
+    private GameObject copy;
+    public LayerMask mask;
+
     void Start()
     {
         cam = Camera.main;
@@ -16,17 +21,34 @@ public class MouseDropper : MonoBehaviour
             Vector3 point = new Vector3(cam.pixelWidth / 2, cam.pixelHeight / 2, 0);
             Ray ray = cam.ScreenPointToRay(point);
             RaycastHit hit;
+
             if (Physics.Raycast(ray, out hit))
             {
-                StartCoroutine(SphereIndicator(hit.point));
+                copy = hit.collider.gameObject;
+                Debug.Log($"Copied {copy}");
             }
         }
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (copy == null)
+            {
+                Debug.Log("You have nothing copied in your clipboard!");
+                return;
+            }
+
+            // Paste object 
+            Vector3 point = new Vector3(cam.pixelWidth / 2, cam.pixelHeight / 2, 0);
+            Ray ray = cam.ScreenPointToRay(point);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                GameObject paste = GameObject.Instantiate(copy);
+                paste.transform.position = hit.point;
+                Debug.Log("Pasted object!");
+            }
+            
+        }
     }
-    private IEnumerator SphereIndicator(Vector3 pos)
-    {
-        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        sphere.transform.position = pos;
-        yield return new WaitForSeconds(1);
-        Destroy(sphere);
-    }
+
 }
